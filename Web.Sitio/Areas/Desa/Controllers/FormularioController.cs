8c7a4b1b218e001c02e8fe0d11dbb21328b88365
unknown <hrledesma@gmail.com>
2018-12-14 00:06:28 -0300

    falta inyeccion de dependencias y un mapper

---
+++ Web.Sitio/Areas/Desa/Controllers/FormularioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Front.Sitio.Areas.Desa.Controllers
+{
+    public class FormularioController : Controller
+    {
+        // GET: Desa/Formulario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
